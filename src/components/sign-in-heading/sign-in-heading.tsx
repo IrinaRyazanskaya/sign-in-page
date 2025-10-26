0acab769981a65f76dd4a5401dfa3cc7ca1867f2
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-10-27 03:20:42 +0500

    Создала компонент заголовок формы

---
+++ src/components/sign-in-heading/sign-in-heading.tsx
@@ -0,0 +1,9 @@
+import "./sign-in-heading.css";
+
+const SignInHeading = () => (
+  <h1 className="sign-in-heading">
+    Sign in to your account to continue
+  </h1>
+);
+
+export default SignInHeading;
