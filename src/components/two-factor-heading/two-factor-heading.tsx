5535b34f14332d3a6eef8b6b8b46387096678750
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-10-28 20:49:36 +0500

    Создала компонент с заголовками второй части формы

---
+++ src/components/two-factor-heading/two-factor-heading.tsx
@@ -0,0 +1,12 @@
+import "./two-factor-heading.css";
+
+export function TwoFactorHeading() {
+  return (
+    <div>
+      <h2 className="two-factor-heading__title">Two-Factor Authentication</h2>
+      <p className="two-factor-heading__subtitle">
+        Enter the 6-digit code from the Google Authenticator app
+      </p>
+    </div>
+  );
+}
